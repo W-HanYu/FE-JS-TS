17b9b0ceec2672ffff79646994cab60e21078d01
wangtao <1453300745@qq.com>
2024-01-25 14:25:35 +0800

    feat: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,25 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
 yarn
 cd vuepress
 yarn
 yarn build
+
+cd .vuepress/dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:W-HanYu/FE-JS-TS.git master:gh-pages
+
+cd -
 
