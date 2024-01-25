bfe05a11e89ae6bc2b166c0992c06b641a86fba8
wangtao <1453300745@qq.com>
2024-01-25 15:52:26 +0800

    fix: main.yml

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,15 @@ yarn build
 cd .vuepress/dist
 
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add .
+# git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# # 如果发布到 https://<USERNAME>.github.io
+# # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:W-HanYu/FE-JS-TS.git master:gh-pages
+# # 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:W-HanYu/FE-JS-TS.git master:gh-pages
 
-cd -
+# cd -
 
